7b4ba2c7e0f4b41dca6a5e96bb9bc404013dc00a
ielgnaw <wuji0223@gmail.com>
2022-04-12 20:49:42 +0800

    feat: bump to 0.0.1-beta.21 issue #1

--- index.d.ts
+++ index.d.ts
@@ -32,3 +32,53 @@ declare module '*.vue' {
   const component: DefineComponent;
   export default component;
 }
+
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.css' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.scss' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.sass' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
