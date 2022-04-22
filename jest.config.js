de48eca2b465474dc459f6727f45b0c8b9ea99f8
liangling <liangling@tencent.com>
2022-04-22 08:09:43 +0800

    feat: resolve workspaces

--- jest.config.js
+++ jest.config.js
@@ -55,7 +55,7 @@ module.exports = {
   // ]
 
   testURL: 'http://localhost/',
-  setupFiles: ['./test-setup.ts'],
+  setupFiles: ['./scripts/cli/test-setup.ts'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
