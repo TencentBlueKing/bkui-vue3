7393e72e38eacda69b801c79569dd3822985b0d5
lockiechen <lockiechen@tencent.com>
2022-04-24 13:41:56 +0800

    docs(button): [button] 完善按钮文档 #145
    affects: @bkui-vue/button, @bkui-vue/loading

---
+++ site/views/button/demo/text.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="basic-button-list">
+    <bk-button
+      text
+      theme="primary"
+    >
+      文字按钮
+    </bk-button>
+    <bk-button
+      text
+      theme="primary"
+      loading
+      loading-mode="spin"
+    >
+      加载中
+    </bk-button>
+    <bk-button
+      text
+      disabled
+    >
+      禁用状态
+    </bk-button>
+  </div>
+</template>
+
+<script setup>
+  // import Loading from '@bkui-vue/loading';
+</script>
+
+<style lang="postcss">
+  .ml10 {
+    margin-left: 10px;
+  }
+</style>
