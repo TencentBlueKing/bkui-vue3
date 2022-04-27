33027c3f1a910bca36c4ddc89bc781ad486fd7f9
huacao <huacao@canway.net>
2022-04-27 17:44:24 +0800

    feature(sideslider): 增加sideslider组件 #1

--- site/router/index.ts
+++ site/router/index.ts
@@ -549,6 +549,18 @@ const routes: RouteRecordRaw[] = [
     },
   },
   /**
+   * @description:侧边栏
+   */
+  {
+  	  path: '/sideslider',
+  	  name: 'sideslider',
+  	  component: () => import('../views/sideslider'),
+  	  meta: {
+  	    group: NavGroupMeta.Feedback,
+  	    navName: 'Sideslider 侧栏',
+  	  },
+  },
+  /**
    * @description:穿梭框
    */
   {
