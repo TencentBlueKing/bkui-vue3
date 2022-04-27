85ce3e84d0fc2a3e0d11a1dca7826afe805459db
ielgnaw <wuji0223@gmail.com>
2022-04-27 20:25:23 +0800

    feat: 单元测试调整 issue #184

--- packages/progress/jest.config.js
+++ packages/progress/jest.config.js
@@ -23,6 +23,7 @@
 * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 * IN THE SOFTWARE.
 */
+
 const baseJestConf = require('../../jest.config');
 
 module.exports = {
