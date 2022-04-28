8d25500c40fdbe29e6f4a6445ee5825155d4f621
vincenttgao <vincenttgao@tencent.com>
2022-04-28 14:55:01 +0800

    bugfix: 使用setup方式写demo issue #8

--- site/views/cascader/check-any-level-demo.vue
+++ site/views/cascader/check-any-level-demo.vue
@@ -1,84 +1,66 @@
 <template>
-  <div
-    class="flex-wrapper"
-  >
-    <bk-cascader
-      v-model="area"
-      :list="list"
-      check-any-level
-    />
-  </div>
+  <bk-cascader
+    v-model="area"
+    :list="list"
+    check-any-level
+  />
 </template>
-<script>
-  import { defineComponent, ref } from 'vue';
+<script setup>
+  import { ref } from 'vue';
 
-  import BkCascader from '@bkui-vue/cascader';
-
-  export default defineComponent({
-    name: 'SiteBreadcrumb',
-    components: {
-      BkCascader,
-    },
-    setup() {
-      const list = [
+  const list = [
+    {
+      id: 'hunan',
+      name: '湖南',
+      disabled: true,
+      children: [
         {
-          id: 'hunan',
-          name: '湖南',
+          id: 'changsha',
+          name: '长沙',
+        },
+        {
+          id: 'yueyang',
+          name: '岳阳',
           disabled: true,
+        },
+      ],
+    }, {
+      id: 'guangxi',
+      name: '广西',
+    }, {
+      id: 'yunnan',
+      name: '云南',
+      children: [
+        {
+          id: 'kunming',
+          name: '昆明',
           children: [
             {
-              id: 'changsha',
-              name: '长沙',
+              id: 'wuhuaqu',
+              name: '五华区阿萨德发送到发送到发到沙发大厦',
             },
             {
-              id: 'yueyang',
-              name: '岳阳',
-              disabled: true,
-            },
-          ],
-        }, {
-          id: 'guangxi',
-          name: '广西',
-        }, {
-          id: 'yunnan',
-          name: '云南',
-          children: [
-            {
-              id: 'kunming',
-              name: '昆明',
-              children: [
-                {
-                  id: 'wuhuaqu',
-                  name: '五华区阿萨德发送到发送到发到沙发大厦',
-                },
-                {
-                  id: 'guanduqu',
-                  name: '官渡区',
-                },
-                {
-                  id: 'xishanqu',
-                  name: '西山区',
-                },
-              ],
+              id: 'guanduqu',
+              name: '官渡区',
             },
             {
-              id: 0,
-              name: '大理',
-            },
-            {
-              id: 'yuxi',
-              name: '玉溪',
+              id: 'xishanqu',
+              name: '西山区',
             },
           ],
         },
-      ];
-      const area = ref([]);
-      return {
-        list,
-        area,
-      };
+        {
+          id: 0,
+          name: '大理',
+        },
+        {
+          id: 'yuxi',
+          name: '玉溪',
+        },
+      ],
     },
-  });
+  ];
+  const area = ref([]);
 
 </script>
 
