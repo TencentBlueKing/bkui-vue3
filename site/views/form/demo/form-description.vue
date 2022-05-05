c0350ef3b139e27744bcec3817d3e2917feeffcd
hailinxiao <hailinxiao@tencent.com>
2022-05-05 14:38:27 +0800

    feature(form): 调整样式 #167

---
+++ site/views/form/demo/form-description.vue
@@ -0,0 +1,66 @@
+<template>
+  <bk-form class="example">
+    <bk-form-item
+      label="姓名"
+      description="中文名"
+    >
+      <bk-input
+        placeholder="请输入"
+        clearable
+      />
+    </bk-form-item>
+    <bk-form-item label="性别">
+      <bk-radio-group>
+        <bk-radio label="男" />
+        <bk-radio label="女" />
+      </bk-radio-group>
+    </bk-form-item>
+    <bk-form-item
+      label="联系方式"
+      description="常用联系方式"
+    >
+      <bk-checkbox-group>
+        <bk-checkbox label="QQ" />
+        <bk-checkbox label="微信" />
+        <bk-checkbox label="Email" />
+      </bk-checkbox-group>
+    </bk-form-item>
+    <bk-form-item label="学历">
+      <bk-select>
+        <bk-option
+          value="1"
+          label="本科以下"
+        />
+        <bk-option
+          value="2"
+          label="本科以上"
+        />
+      </bk-select>
+    </bk-form-item>
+    <bk-form-item label="介绍">
+      <bk-input
+        placeholder="请输入"
+        type="textarea"
+      />
+    </bk-form-item>
+    <bk-form-item>
+      <bk-button theme="primary">
+        提交
+      </bk-button>
+    </bk-form-item>
+  </bk-form>
+</template>
+<script setup>
+  import BkButton from '@bkui-vue/button';
+  import BkCheckbox, { BkCheckboxGroup } from '@bkui-vue/checkbox';
+  import BkForm, { BkFormItem } from '@bkui-vue/form';
+  import BkInput from '@bkui-vue/input';
+  import BkRadio, { BkRadioGroup } from '@bkui-vue/radio';
+  import BkSelect, { BkOption } from '@bkui-vue/select';
+
+</script>
+<style lang="postcss" scoped>
+  .example {
+    max-width: 800px;
+  }
+</style>
