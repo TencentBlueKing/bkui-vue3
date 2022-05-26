6a3fce3cfdf2f8ab66fa465e73f905732b76e404
hailinxiao <hailinxiao@tencent.com>
2022-05-26 20:15:58 +0800

    bugfix(radio-group): 默认选中状态 #330

--- site/views/radio/demo/radio-button.vue
+++ site/views/radio/demo/radio-button.vue
@@ -2,11 +2,9 @@
   <bk-radio-group
     v-model="radioGroupValue"
   >
-    <template v-if="show">
-      <bk-radio-button label="QQ" />
-      <bk-radio-button label="微信" />
-      <bk-radio-button label="Email" />
-    </template>
+    <bk-radio-button label="QQ" />
+    <bk-radio-button label="微信" />
+    <bk-radio-button label="Email" />
   </bk-radio-group>
 </template>
 <script setup>
@@ -14,10 +12,5 @@
 
   const radioGroupValue = ref('微信');
 
-  const show = ref(false);
-
-  setTimeout(() => {
-    show.value = true;
-  }, 3000);
 </script>
 
