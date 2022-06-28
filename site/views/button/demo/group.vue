f51ba1f04c9a8096ce1e19ea1224c1dd7d2709c2
lockiechen <lockiechen@tencent.com>
2022-06-28 10:44:04 +0800

    fix(button | input): fix style #55
    affects: @bkui-vue/button, @bkui-vue/input, @bkui-vue/styles

--- site/views/button/demo/group.vue
+++ site/views/button/demo/group.vue
@@ -2,11 +2,36 @@
   <div class="button-group-list">
     <div class="mb10">
       <bk-button-group size="small">
-        <bk-button>北京</bk-button>
-        <bk-button>上海</bk-button>
-        <bk-button>广州</bk-button>
-        <bk-button>深圳</bk-button>
-        <bk-button>
+        <bk-button
+          :selected="selected === '北京'"
+          @click="handleSelected('北京')"
+        >
+          北京
+        </bk-button>
+        <bk-button
+          :selected="selected === '上海'"
+          disabled
+          @click="handleSelected('上海')"
+        >
+          上海
+        </bk-button>
+        <bk-button
+          :selected="selected === '广州'"
+          @click="handleSelected('广州')"
+        >
+          广州
+        </bk-button>
+        <bk-button
+          :selected="selected === '深圳'"
+          disabled
+          @click="handleSelected('深圳')"
+        >
+          深圳
+        </bk-button>
+        <bk-button
+          :selected="selected === '其他'"
+          @click="handleSelected('其他')"
+        >
           其他
           <angle-down />
         </bk-button>
@@ -15,11 +40,34 @@
 
     <div class="mb10">
       <bk-button-group>
-        <bk-button>北京</bk-button>
-        <bk-button>上海</bk-button>
-        <bk-button>广州</bk-button>
-        <bk-button>深圳</bk-button>
-        <bk-button>
+        <bk-button
+          :selected="selected === '北京'"
+          @click="handleSelected('北京')"
+        >
+          北京
+        </bk-button>
+        <bk-button
+          :selected="selected === '上海'"
+          @click="handleSelected('上海')"
+        >
+          上海
+        </bk-button>
+        <bk-button
+          :selected="selected === '广州'"
+          @click="handleSelected('广州')"
+        >
+          广州
+        </bk-button>
+        <bk-button
+          :selected="selected === '深圳'"
+          @click="handleSelected('深圳')"
+        >
+          深圳
+        </bk-button>
+        <bk-button
+          :selected="selected === '其他'"
+          @click="handleSelected('其他')"
+        >
           其他
           <angle-down />
         </bk-button>
@@ -28,11 +76,34 @@
 
     <div>
       <bk-button-group size="large">
-        <bk-button>北京</bk-button>
-        <bk-button>上海</bk-button>
-        <bk-button>广州</bk-button>
-        <bk-button>深圳</bk-button>
-        <bk-button>
+        <bk-button
+          :selected="selected === '北京'"
+          @click="handleSelected('北京')"
+        >
+          北京
+        </bk-button>
+        <bk-button
+          :selected="selected === '上海'"
+          @click="handleSelected('上海')"
+        >
+          上海
+        </bk-button>
+        <bk-button
+          :selected="selected === '广州'"
+          @click="handleSelected('广州')"
+        >
+          广州
+        </bk-button>
+        <bk-button
+          :selected="selected === '深圳'"
+          @click="handleSelected('深圳')"
+        >
+          深圳
+        </bk-button>
+        <bk-button
+          :selected="selected === '其他'"
+          @click="handleSelected('其他')"
+        >
           其他
           <angle-down />
         </bk-button>
@@ -42,7 +113,13 @@
 </template>
 
 <script setup>
+  import { ref } from 'vue';
+
   import { AngleDown } from '@bkui-vue/icon';
+  const selected = ref('深圳');
+  function handleSelected(v) {
+    selected.value = v;
+  }
 </script>
 
 <style lang="postcss">
