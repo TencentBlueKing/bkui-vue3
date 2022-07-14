6793eeed4c33dbd65ed181d42d8480327760cd26
xintaoLi <li18566230175@gmail.com>
2022-07-14 15:40:24 +0800

    feature(popover2): 支持弹出内容mouseenter选中 #468

--- site/views/popover2/demo/always.vue
+++ site/views/popover2/demo/always.vue
@@ -1,10 +1,12 @@
 <template>
   <bk-popover2
     always
+    boundary="#oooxxxvvv"
     placement="right"
     width="300"
     content="这里是提示文字当鼠标经过这段文字时，会显示一个气泡框当鼠标经过这段文字时，会显示一个气泡框当鼠标经过这段文字时"
   >
     <bk-button>总是显示</bk-button>
   </bk-popover2>
+  <div id="oooxxxvvv" />
 </template>
