f0e1ccc0516c93374dd421f24ddea3da5a4c2fe8
ptaoapeng <1127949533@qq.com>
2022-07-18 17:53:04 +0800

    feat(color-picker): 添加color-picker组件 #480

---
+++ site/views/color-picker/preset-demo.vue
@@ -0,0 +1,25 @@
+<template>
+  <bk-color-picker
+    v-model="color3"
+    class="color-picker"
+    :recommend="false"
+  />
+  <bk-color-picker
+    v-model="color3"
+    :recommend="recommendColors"
+  />
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  import BkColorPicker from '@bkui-vue/color-picker';
+  const color3 = ref('3A84FF');
+  const recommendColors = ref(['#000', '#333', '#666', '#999', 'FFF']);
+</script>
+
+<style lang="postcss" scoped>
+.color-picker {
+  margin-right: 20px;
+}
+</style>
---
+++ site/views/color-picker/size-demo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <bk-color-picker
+      v-model="color2"
+      class="color-picker"
+      size="large"
+    />
+    <bk-color-picker
+      v-model="color2"
+      class="color-picker"
+    />
+    <bk-color-picker
+      v-model="color2"
+      size="small"
+    />
+  </div>
+</template>
+<script setup>
+  import { ref } from 'vue';
+
+  import BkColorPicker from '@bkui-vue/color-picker';
+  const color2 = ref('3A84FF');
+</script>
+
+<style lang="postcss" scoped>
+.color-picker {
+  margin-right: 20px;
+}
+</style>
