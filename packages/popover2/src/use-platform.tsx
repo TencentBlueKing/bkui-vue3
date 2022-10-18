4897e538f663a5d2e8826d0189a75bc656160c6b
israelli <israelli@tencent.com>
2022-10-18 16:05:16 +0800

    bugfix(popover2): shadowRoot cannot work with fullscreen #643

--- packages/popover2/src/use-platform.tsx
+++ packages/popover2/src/use-platform.tsx
@@ -38,12 +38,12 @@ export default (fullscreenTarget?: HTMLElement) => {
   };
 
   const getClippingRect = ({ }) => {
-    const { width = 0, height = 0 } = fullscreenTarget?.getBoundingClientRect() ?? {};
+    const { width = 0, height = 0, left, top } = fullscreenTarget?.getBoundingClientRect() ?? {};
     return {
       width,
       height,
-      x: 0,
-      y: 0,
+      x: `${left}px`,
+      y: `${top}px`,
     };
   };
 
