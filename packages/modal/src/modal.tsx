9a8482fb79d41336f8f28d08798016e815080541
israelli <israelli@tencent.com>
2022-10-27 21:32:46 +0800

    bugfix(dialog|sideslider): 弹窗和侧栏多层嵌套关闭时计算遮罩&zIndex错误 #678

--- packages/modal/src/modal.tsx
+++ packages/modal/src/modal.tsx
@@ -26,7 +26,7 @@
 
 import { defineComponent, Transition } from 'vue';
 
-import { BKPopIndexManager } from '@bkui-vue/shared';
+import { bkPopIndexManager } from '@bkui-vue/shared';
 
 import { propsMixin } from './props.mixin';
 
@@ -39,7 +39,6 @@ export default defineComponent({
   data() {
     return {
       visible: false,
-      bkPopIndexManager: null,
       closeTimer: null,
     };
   },
@@ -87,8 +86,8 @@ export default defineComponent({
     },
     visible(val: boolean) {
       if (val) {
-        this.bkPopIndexManager = new BKPopIndexManager();
-        this.bkPopIndexManager.onMaskClick((_e: MouseEvent) => {
+        // this.bkPopIndexManager = new BKPopIndexManager();
+        bkPopIndexManager.onMaskClick((_e: MouseEvent) => {
           this.handleClickOutSide();
         });
         this.$nextTick(() => {
@@ -96,19 +95,21 @@ export default defineComponent({
             'background-color': 'rgba(0,0,0,0)',
           };
           const appendStyle = this.showMask ? {} : hideMaskStyle;
-          this.bkPopIndexManager.show(this.$el, this.showMask, appendStyle, this.transfer, this.zIndex);
+          bkPopIndexManager.show(this.$el, this.showMask, appendStyle, this.transfer, this.zIndex);
           this.$emit('shown');
         });
       } else {
-        this.bkPopIndexManager?.hide(this.$el, this.transfer);
-        this.bkPopIndexManager?.destroy();
+        bkPopIndexManager?.hide(this.$el, this.transfer);
+        bkPopIndexManager?.destroy();
       }
     },
   },
 
   beforeUnmount() {
-    this.bkPopIndexManager?.hide(this.$el);
-    this.bkPopIndexManager?.destroy();
+    if (this.visible) {
+      bkPopIndexManager?.hide(this.$el);
+      bkPopIndexManager?.destroy();
+    }
   },
   methods: {
     handleClickOutSide() {
