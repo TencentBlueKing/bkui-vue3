510f451563e57eb4a331a8d1027ed8824b8d0366
hmohuang <13407923955@163.com>
2022-12-02 15:34:30 +0800

    fix:修改符号

--- site/components/demo-box.tsx
+++ site/components/demo-box.tsx
@@ -144,7 +144,7 @@ export default defineComponent({
           <div class="example-tools">
             {this.desc}
               <BoxIcon tips='执行' style={{ marginLeft: 'auto'}}  >
-                  <Stackblitz code={this.activeCode} style={{ width: '100%', height: '100%', textAlign: "center", lineHeight: "23px" }}/>
+                  <Stackblitz code={this.activeCode} style={{ width: "100%", height: "100%", textAlign: "center", lineHeight: "23px" }}/>
               </BoxIcon>
             <BoxIcon
               tips='代码'
