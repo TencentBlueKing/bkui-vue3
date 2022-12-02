47f183c99ebb55d50393847f27233f4ffdcc888a
israelli <israelli@tencent.com>
2022-12-02 16:53:38 +0800

    feat: 增加tree示例 #720

--- site/views/tree/node-content-action.vue
+++ site/views/tree/node-content-action.vue
@@ -1,23 +1,44 @@
 <template>
   <div style="width: 100%; overflow: auto">
     <div class="row">
-      <div class="cell">
-        <span class="attr-tag">nodeContentAction = ['selected', 'expand', 'click']</span>
-        <bk-tree
-          :data="treeData"
-          label="name"
-          children="children"
-          :node-content-action="['selected', 'click']"
-        />
+      <div class="column">
+        <div class="attr-tag">
+          node-content-action = ['selected', 'click', 'expand'] | (默认配置)
+        </div>
+        <div class="cell">
+          <bk-tree
+            :data="treeData"
+            label="name"
+            children="children"
+            :node-content-action="['selected', 'click', 'expand']"
+          />
+        </div>
       </div>
-      <div class="cell">
-        <span class="attr-tag">nodeContentAction =[]</span>
-        <bk-tree
-          :data="treeData"
-          label="name"
-          :node-content-action="[]"
-          children="children"
-        />
+      <div class="column">
+        <div class="attr-tag">
+          node-content-action =['selected', 'click']
+        </div>
+        <div class="cell">
+          <bk-tree
+            :data="treeData"
+            label="name"
+            :node-content-action="['selected', 'click']"
+            children="children"
+          />
+        </div>
+      </div>
+      <div class="column">
+        <div class="attr-tag">
+          node-content-action =[] | (如果要禁用所有行为请设置为[],不要设置null|undefined之类，这类设置会被置换为默认配置)
+        </div>
+        <div class="cell">
+          <bk-tree
+            :data="treeData"
+            label="name"
+            :node-content-action="[]"
+            children="children"
+          />
+        </div>
       </div>
     </div>
   </div>
@@ -40,12 +61,15 @@
 .row {
   display: flex;
   width: 100%;
-  height: 300px;
   padding-top: 15px;
-  overflow: auto;
+}
+
+.column {
+  width: 33%;
 }
 
 .cell {
+  height: 300px;
   padding: 0 15px;
   overflow: auto;
   border-right: solid 1px #ddd;
@@ -53,9 +77,8 @@
 }
 
 .attr-tag {
-  display: inline-block;
-  width: 100%;
   padding: 10px;
+  margin-right: 2px;
   background: #f0f1f5;
 }
 </style>
