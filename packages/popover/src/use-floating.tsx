193316540ccdc2ea689093182997acafb95f97bc
israelli <li18566230175@gmail.com>
2023-05-10 20:30:20 +0800

    fix(modal & table): 修复表格cell文本溢出&弹出遮罩透传 #487

--- packages/popover/src/use-floating.tsx
+++ packages/popover/src/use-floating.tsx
@@ -303,8 +303,12 @@ export default (props: PopoverPropTypes, ctx, { refReference, refContent, refArr
     const delay = resolvePopoverDelay()[0];
     // 设置settimeout避免hidePopover导致显示问题
     popShowTimerId = setTimeout(() => {
-      popHideTimerId && clearTimeout(popHideTimerId);
-      !props.disabled && (localIsShow.value = true);
+      if (popHideTimerId) {
+        clearTimeout(popHideTimerId);
+      }
+      if (!props.disabled) {
+        localIsShow.value = true;
+      }
     }, delay);
   };
 
