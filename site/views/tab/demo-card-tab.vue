e7dbb277b493a1515fc9a13059807bfa117af820
andyljzhou <1@zhoulujun.cn>
2023-05-16 11:27:15 +0800

    docs: tab文档更新  issue #859

---
+++ site/views/tab/demo-card-tab.vue
@@ -0,0 +1,34 @@
+<template>
+  <div style="padding: 10px 20px;background: #f5f7fa;">
+    <bk-tab
+      v-model:active="active"
+      type="card-tab"
+    >
+      <bk-tab-panel
+        v-for="(item,index) in panels"
+        :key="item.name"
+        :name="item.name"
+        :label="item.label"
+      >
+        {{ item.label }}-{{ index }}
+      </bk-tab-panel>
+    </bk-tab>
+  </div>
+</template>
+<script>
+  import { defineComponent } from 'vue';
+  export default defineComponent({
+    components: {},
+    data() {
+      return {
+        panels: [
+          { name: 'mission', label: '任务报表', count: 10 },
+          { name: 'config', label: '加速配置', count: 20 },
+          { name: 'history', label: '历史版本', count: 30 },
+          { name: 'deleted', label: '已归档加速任务', count: 40 },
+        ],
+        active: 'mission',
+      };
+    },
+  });
+</script>
