aaac459234d36734e8dbaae8a49022fb7c791199
ielgnaw <wuji0223@gmail.com>
2023-06-01 01:59:23 +0800

    feat: date-picker 组件支持 year 和 month 模式 && bump to 0.0.1-beta.424

---
+++ site/views/date-picker/year-month-demo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p style="font-size: 14px;">
+      年选择器 {{ yearValue }}
+    </p>
+    <bk-date-picker
+      v-model="initDateTime"
+      type="year"
+      @change="handleYearChange"
+    />
+  </div>
+  <div>
+    <p style="font-size: 14px;">
+      月选择器 {{ monthValue }}
+    </p>
+    <bk-date-picker
+      v-model="initDateTime"
+      type="month"
+      @change="handleMonthChange"
+    />
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  const initDateTime = ref(new Date());
+  const yearValue = ref('');
+  const handleYearChange = (val) => {
+    console.warn('handleChange', val);
+    yearValue.value = val;
+  };
+  const monthValue = ref('');
+  const handleMonthChange = (val) => {
+    console.warn('handleChange', val);
+    monthValue.value = val;
+  };
+</script>
