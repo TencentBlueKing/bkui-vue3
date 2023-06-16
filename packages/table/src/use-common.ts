98e84382b51d19d575599d6018c8e562582b8f22
andyljzhou <1@zhoulujun.cn>
2023-06-16 19:59:38 +0800

    perf: 表格高度计算  issue #1006

--- packages/table/src/use-common.ts
+++ packages/table/src/use-common.ts
@@ -98,7 +98,15 @@ export const useClass = (props: TablePropTypes, targetColumns: Column[], root?, 
     [resolveClassName('table-footer')]: true,
     ['is-hidden']: !props.pagination || !props.data.length,
   }));
-
+  const getTableHeight = (): string => {
+    if (props.height === 'number') {
+      return `${props.height}px`;
+    }
+    if (typeof props.height === 'string') {
+      return props.height;
+    }
+    return 'auto';
+  };
   const resolveWidth = () => {
     // const columns = getColumns();
     if (resolvedColumns.value.every((col: Column) => /^\d+\.?\d*(px)?$/ig.test(`${col.width}`))) {
@@ -115,8 +123,8 @@ export const useClass = (props: TablePropTypes, targetColumns: Column[], root?, 
     minHeight: resolveNumberOrStringToPix(props.minHeight, 'auto'),
     width: resolveWidth(),
     maxWidth: '100%',
-    // 如果表格已经设置了高度，父容器直接给定高度，先撑开，避免重绘
-    ...(typeof props.height === 'number' ? { height: `${props.height}px` } : null),
+    //
+    height: getTableHeight(),
     // height: resolveNumberOrStringToPix(props.height, 'auto')
   }));
 
