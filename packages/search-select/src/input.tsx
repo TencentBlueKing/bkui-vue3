12c162ee4aa374174d4b560e001ec5592a6c5717
ielgnaw <wuji0223@gmail.com>
2023-08-10 16:30:40 +0800

    feat: bump to 0.0.2-beta.4 && 样式前缀处理完成

--- packages/search-select/src/input.tsx
+++ packages/search-select/src/input.tsx
@@ -306,7 +306,6 @@ export default defineComponent({
       onValidate('');
     }
     async function handleSelectItem(item: ICommonItem, type?: SearchItemType) {
-      console.info(item, '==========');
       // 快捷选中
       if (item.value?.id) {
         if ((props.valueBehavior === ValueBehavior.NEEDKEY && item.value) || !props.validateValues) {
@@ -321,8 +320,12 @@ export default defineComponent({
         }
         usingItem.value = new SelectedItem({ ...item, id: item.realId ?? item.id }, type, valueSplitCode.value);
         usingItem.value.addValue(item.value);
-        const res = await validateUsingItemValues(item);
-        if (!res) return;
+        const res = await validateUsingItemValues(item.value);
+        if (!res) {
+          usingItem.value = null;
+          return;
+        };
+
         setSelectedItem(usingItem.value);
         menuHoverId.value = '';
         setInputFocus(true);
