a3976002d7cdd1bd8cb1226371b635c01bce61f7
andyljzhou <1@zhoulujun.cn>
2023-08-15 11:35:39 +0800

    feat(color-pick): [color-pick]添加trigger插件 #1169

---
+++ site/views/color-picker/slot-demo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <bk-color-picker
+      v-model="color1"
+      :show-value="false"
+    >
+      <template #trigger="{value,isShow}">
+        <div>
+         <div>value:<span>{{value}}</span></div>
+         <div>isShow:<span>{{String(isShow)}}</span></div>
+        </div>
+      </template>
+    </bk-color-picker>
+  </div>
+</template>
+<script setup>
+  import { ref } from 'vue';
+
+  import BkColorPicker from '@bkui-vue/color-picker';
+  const color1 = ref('');
+  const change = (v) => {
+    console.log(v);
+  };
+</script>
+
+<style lang="postcss" scoped>
+.color-picker {
+  margin-right: 20px;
+}
+</style>
