1b7edc3e40a6a5499d999b712e5d27eca7c616c2
lockiechen <33082528+lockiechen@users.noreply.github.com>
2023-11-22 17:47:41 +0800

    feature(input): 数字输入框enter事件value值返回int类型；autofocus失效问题；数字输入框未输入时，直接点击后面的控制区域，会直接填充最小值或者0 #1262

--- site/views/input/demo/number.vue
+++ site/views/input/demo/number.vue
@@ -10,7 +10,7 @@
       clearable
     />
     <bk-input
-      v-model="val"
+      v-model="val1"
       class="mb10"
       type="number"
       :min="1"
@@ -34,7 +34,8 @@
 
 <script setup>
   import { ref } from 'vue';
-  const val = ref(0);
+  const val = ref();
+  const val1 = ref(1);
 </script>
 
 <style lang="less">
