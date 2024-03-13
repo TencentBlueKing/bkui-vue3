863558520b625d54428e729453ca78473cecbcd3
ielgnaw <wuji0223@gmail.com>
2024-03-13 16:38:40 +0800

    feat: fork 0.0.3-beta.2, for bkvision # Reviewed, transaction id: 3796

--- site/views/table/column-template.vue
+++ site/views/table/column-template.vue
@@ -2,6 +2,7 @@
   <section>
     <bk-table
       :data="tableData"
+      :settings="settings"
     >
       <template
         v-for="column in columns"
@@ -11,6 +12,7 @@
           :label="column.label"
           :type="column.type"
           :field="column.field"
+          :width="column.width"
         />
       </template>
     </bk-table>
@@ -27,20 +29,46 @@
           {
             label: '序号',
             type: 'index',
+            width: '120px',
           },
           {
             label: '名称/内网IP',
             field: 'ip',
+            width: '320px',
           },
           {
             label: '来源',
             field: 'source',
+            width: '120px',
           },
           {
             label: '创建时间',
             field: 'create_time',
+            width: '220px',
           },
         ],
+        settings: {
+          trigger: 'click',
+          fields: [
+            {
+              name: '序号',
+              id: 'index',
+            },
+            {
+              name: '名称/内网IP',
+              id: 'ip',
+            },
+            {
+              name: '来源',
+              id: 'source',
+            },
+            {
+              name: '创建时间',
+              id: 'create_time',
+            },
+          ],
+          checked: ['ip', 'index'],
+        },
       };
     },
   };
