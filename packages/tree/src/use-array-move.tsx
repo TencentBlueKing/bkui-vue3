de9a4d254149e5966a2424ff63ed37fa74db6891
xintaoLi <li18566230175@gmail.com>
2025-09-19 11:00:49 +0800

    bugfix(tree): 拖拽排序功能替换节点位置不对 #2466 # Reviewed, transaction id: 58592

--- packages/tree/src/use-array-move.tsx
+++ packages/tree/src/use-array-move.tsx
@@ -2,8 +2,6 @@
  * Tencent is pleased to support the open source community by making
  * 蓝鲸智云PaaS平台 (BlueKing PaaS) available.
  *
- * Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
- *
  * 蓝鲸智云PaaS平台 (BlueKing PaaS) is licensed under the MIT License.
  *
  * License for 蓝鲸智云PaaS平台 (BlueKing PaaS):
