3a7eb4657938533c6bbec6da1b2c250a9c4ad50a
xuzhan <xuzhan@tencent.com>
2025-10-16 20:50:46 +0800

    优化组件库文档 # Reviewed, transaction id: 60837

--- site/lib/client/src/components/render-api-documentation/index.vue
+++ site/lib/client/src/components/render-api-documentation/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="documentation-component">
+  <section class="documentation-component g-scrollbar">
     <!-- 主内容区域 -->
     <main class="content">
       <!-- 主组件配置 -->
@@ -93,97 +93,97 @@
 </template>
 
 <script lang="ts" setup>
-  import { computed, ref, watch } from 'vue';
-  import { useRoute } from 'vue-router';
-  import { IComponentWiki } from '@/types/component';
-  import RenderTable from './render-table/index.vue';
-  import useStorage from '@/hooks/use-storage';
-  import { ANCHOR_KEY } from '@/types/contants';
+import { computed, ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import { IComponentWiki } from '@/types/component';
+import RenderTable from './render-table/index.vue';
+import useStorage from '@/hooks/use-storage';
+import { ANCHOR_KEY } from '@/types/contants';
 import { capitalizeWord } from '@/common/util';
 
-  interface IProps {
-    component: object;
-    componentWiki: IComponentWiki;
-    activeComponent: IComponentWiki | null;
-  }
-  const props = defineProps<IProps>();
-  // 路由相关
-  const route = useRoute();
-  // 存储方法
-  const { removeStorage } = useStorage();
+interface IProps {
+  componentWiki: IComponentWiki;
+  activeComponent: IComponentWiki | null;
+}
+const props = defineProps<IProps>();
 
-  // 分类关键字
-  const categoryKeyword = {
-    props: '属性',
-    emits: '事件',
-    slots: '插槽',
-  };
+// 路由相关
+const route = useRoute();
+// 存储方法
+const { removeStorage } = useStorage();
 
-  // 组件基础类型映射
-  const componentBaseTypes = [
-    { key: 'props', categoryKey: categoryKeyword.props },
-    { key: 'emits', categoryKey: categoryKeyword.emits },
-    { key: 'slots', categoryKey: categoryKeyword.slots },
-  ];
+// 分类关键字
+const categoryKeyword = {
+  props: '属性',
+  emits: '事件',
+  slots: '插槽',
+};
 
-  // 侧边栏默认激活第一项
-  const activeAnchor = ref(`${props.activeComponent.name}${capitalizeWord(componentBaseTypes[0].key)}`);
+// 组件基础类型映射
+const componentBaseTypes = [
+  { key: 'props', categoryKey: categoryKeyword.props },
+  { key: 'emits', categoryKey: categoryKeyword.emits },
+  { key: 'slots', categoryKey: categoryKeyword.slots },
+];
 
-  // 子组件配置
-  const childrenConfigs = computed(() => props.componentWiki.children ?? []);
+// 侧边栏默认激活第一项
+const activeAnchor = ref(`${props.activeComponent.name}${capitalizeWord(componentBaseTypes[0].key)}`);
 
-  // 计算导航项数据
-  const navItems = computed(() => {
-    const items: { id: string; title: string }[] = [];
-    // 处理主组件的props/emits/slots
-    Object.keys(categoryKeyword).forEach(key => {
-      if (props.componentWiki[key]) {
-        items.push({
-          id: `${props.activeComponent.name}${capitalizeWord(key)}`,
-          title: `${props.componentWiki.title} ${categoryKeyword[key]}`,
-        });
-      }
-    });
-    // 处理types类型定义
-    if (props.componentWiki.types) {
-      props.componentWiki.types.forEach(item => {
-        items.push({ id: item.name, title: item.name });
+// 子组件配置
+const childrenConfigs = computed(() => props.componentWiki.children ?? []);
+
+// 计算导航项数据
+const navItems = computed(() => {
+  const items: { id: string; title: string }[] = [];
+  // 处理主组件的props/emits/slots
+  Object.keys(categoryKeyword).forEach(key => {
+    if (props.componentWiki[key]) {
+      items.push({
+        id: `${props.activeComponent.name}${capitalizeWord(key)}`,
+        title: `${props.componentWiki.title} ${categoryKeyword[key]}`,
       });
     }
-    // 处理子组件的配置
-    if (childrenConfigs.value.length) {
-      childrenConfigs.value.forEach(child => {
-        Object.keys(categoryKeyword).forEach(key => {
-          if (child[key]) {
-            items.push({
-              id: `${child.name}${capitalizeWord(key)}`,
-              title: `${child.name} ${categoryKeyword[key]}`,
-            });
-          }
-        });
-      });
-    }
-    return items;
   });
+  // 处理types类型定义
+  if (props.componentWiki.types) {
+    props.componentWiki.types.forEach(item => {
+      items.push({ id: item.name, title: item.name });
+    });
+  }
+  // 处理子组件的配置
+  if (childrenConfigs.value.length) {
+    childrenConfigs.value.forEach(child => {
+      Object.keys(categoryKeyword).forEach(key => {
+        if (child[key]) {
+          items.push({
+            id: `${child.name}${capitalizeWord(key)}`,
+            title: `${child.name} ${categoryKeyword[key]}`,
+          });
+        }
+      });
+    });
+  }
+  return items;
+});
 
-  watch(
-    route,
-    newRoute => {
-      activeAnchor.value =
-        newRoute.hash?.replace('#', '') || `${props.activeComponent.name}${capitalizeWord(componentBaseTypes[0].key)}`;
-    },
-    { deep: true },
-  );
+watch(
+  route,
+  newRoute => {
+    activeAnchor.value = newRoute.hash?.replace('#', '') || `${props.activeComponent.name}${capitalizeWord(componentBaseTypes[0].key)}`;
+  },
+  { deep: true },
+);
 </script>
 
 <style lang="postcss" scoped>
   .documentation-component {
     display: flex;
     width: 100%;
-    height: 100%;
+    height: calc(100% - 149px);
     gap: 24px;
     overflow: auto;
     scroll-behavior: smooth;
+    padding: 16px 8px 40px 34px;
   }
 
   /* 子组件样式 */
--- site/lib/client/src/views/children/component/children/api.vue
+++ site/lib/client/src/views/children/component/children/api.vue
@@ -1,60 +1,14 @@
 <template>
-  <bk-loading
-    :loading="loading"
-    class="demo-home"
-  >
-    <render-api-documentation
-      :component="component"
-      :component-wiki="componentStore.activeComponentWiki"
-      :active-component="componentStore.activeComponentWiki"
-    />
-  </bk-loading>
+  <render-api-documentation
+    :component-wiki="componentStore.activeComponentWiki"
+    :active-component="componentStore.activeComponentWiki"
+  />
 </template>
 
 <script lang="ts" setup>
-  import { Loading as BkLoading } from 'bkui-vue';
-  import { onBeforeMount, onBeforeUnmount, ref, shallowRef, watch } from 'vue';
+import RenderApiDocumentation from '@/components/render-api-documentation/index.vue';
+import { useComponent } from '@/store/component';
 
-  import RenderApiDocumentation from '@/components/render-api-documentation/index.vue';
-  import { getComponent, getCss } from '@/http/api';
-  import { useComponent } from '@/store/component';
-
-  const component = shallowRef();
-
-  // 获取组件
-  const componentStore = useComponent();
-  const loading = ref(false);
-
-  /**
-   * @description 更新组件
-   */
-  const handleGetComponent = () => {
-    if (componentStore.activeComponentWiki) {
-      loading.value = true;
-      Promise.all([
-        getComponent(componentStore.activeComponentWiki.name, 'dev'),
-        getCss(componentStore.activeComponentWiki.name, 'dev'),
-      ])
-        .then(() => {
-          component.value = window.getComponent().default;
-        })
-        .finally(() => {
-          loading.value = false;
-        });
-    }
-  };
-
-  watch(() => componentStore.activeComponentWiki, handleGetComponent);
-
-  onBeforeMount(() => {});
-
-  onBeforeUnmount(() => {});
+// 获取组件
+const componentStore = useComponent();
 </script>
-
-<style lang="postcss" scoped>
-  .demo-home {
-    width: 100%;
-    height: calc(100% - 149px);
-    padding: 16px 8px 40px 34px;
-  }
-</style>
