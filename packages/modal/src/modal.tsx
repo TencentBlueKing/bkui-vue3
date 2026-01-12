50dd864ed049782e382f656256dfa9e32fe7ec3f
jojoway <3096241994@qq.com>
2026-01-12 10:06:05 +0800

    bugfix(select): 点击Mask不关闭下拉面板 #2657 # Reviewed, transaction id: 71212

--- packages/modal/src/modal.tsx
+++ packages/modal/src/modal.tsx
@@ -113,8 +113,11 @@ export default defineComponent({
     };
 
     const handleClickOutSide = (e: MouseEvent) => {
-      e.stopImmediatePropagation();
-      e.stopPropagation();
+      const target = e.target as HTMLElement;
+      // 判断点击的是否是当前 Dialog 的 mask（而不是子 Dialog 的 mask）
+      if (target !== maskRef.value) {
+        return; // 不是当前 Dialog 的 mask，不处理
+      }
       e.preventDefault();
 
       if (props.quickClose) {
